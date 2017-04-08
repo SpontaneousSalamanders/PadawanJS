0b5c2b092b50add9766ec2ebd63e9189ddab7bd0
Chris Chu <cchu008@gmail.com>
2017-04-07 21:59:25 -0700

    Update readme, serve static files, hello world, test for heroku pipeline setup

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/../client/dist'));
 
 var server = app.listen(port, function() {
   console.log('App is listening on port: ', port);
