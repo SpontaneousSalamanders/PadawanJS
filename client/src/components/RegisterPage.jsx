5273e149067d88a9fb651e831f3be3d7eb6bc29a
Chris Chu <cchu008@gmail.com>
2017-04-10 16:38:14 -0700

    initial commit starting sign up page

---
+++ client/src/components/RegisterPage.jsx
@@ -0,0 +1,13 @@
+/*
+ * RegisterPage
+ *
+ * Users login on this page
+ * Route: /register
+ *
+ */
+
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+// import Form from '../Form.react';
+// import { sendingRequest, register } from '../../actions/AppActions';
+// import LoadingIndicator from '../LoadingIndicator.react';
