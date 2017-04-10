9d97a8e17c6f611a2fbbf58d89f1018d5afb2475
Your Name <ddeng343@gmail.com>
2017-04-10 16:08:40 -0700

    (feat) Created dummy data to populate database

---
+++ server/db/dummyData.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = [
+  {name: 'Beth Johnson', location: 'SF', techStack: ['React', 'Express', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/xEUUtut.jpg'},
+  {name: 'Benji Marinacci', location: 'Redwood City', techStack: ['React', 'Express', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/iwn6mV5.jpg'},
+  {name: 'Fred Zirdung', location: 'SF', techStack: ['React', 'Express', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/9awfsn2.jpg'},
+  {name: 'Eric Brown', location: 'Albany', techStack: ['React', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/adDa5JU.jpg'},
+  {name: 'Tyler Arbus', location: 'Toronto', techStack: ['Angular', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/ohFukM2.jpg'},
+  {name: 'Paul Mills', location: 'SF', techStack: ['React', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/9eJRSav.jpg'},
+  {name: 'Alison Zhang', location: 'Palo Alto', techStack: ['React', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/uz1C3om.jpg'},
+  {name: 'Jong Kim', location: 'San Jose', techStack: ['React', 'Node.js', 'JavaScript'], picture: 'http://imgur.com/yTpcGqk.jpg'},
+]
