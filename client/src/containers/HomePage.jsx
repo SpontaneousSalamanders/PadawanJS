488911589936af412547ff7d9933d47d9a0abdd9
Chris Chu <cchu008@gmail.com>
2017-04-10 21:02:12 -0700

    work on homepage

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -6,3 +6,28 @@
  *
  */
 
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import Nav from '../containers/Nav.jsx';
+import { connect } from 'react-redux';
+
+class HomePage extends Component {
+  render() {
+    const dispatch = this.props.dispatch;
+    const { loggedIn } = this.props.data;
+
+    return (
+
+
+    );
+  }
+}
+
+function select(state) {
+  return {
+    data: state
+  };
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(select)(HomePage);
