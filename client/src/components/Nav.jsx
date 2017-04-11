bd967d8546aef44886129a23dca30c5cd47e6901
Chris Chu <cchu008@gmail.com>
2017-04-11 14:18:35 -0700

    working on Login Page bug

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -42,9 +42,9 @@ class Nav extends Component {
   }
 }
 
-Nav.propTypes = {
-  loggedIn: React.PropTypes.bool.isRequired,
-  currentlySending: React.PropTypes.bool.isRequired
-}
+// Nav.propTypes = {
+//   loggedIn: React.PropTypes.bool.isRequired,
+//   currentlySending: React.PropTypes.bool.isRequired
+// }
 
 export default Nav;
