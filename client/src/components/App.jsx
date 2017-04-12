a02bc6e4446f1f6cecaa42a73c411a1fb48e1daf
Kay Lee <me@kalbito.com>
2017-04-11 17:03:25 -0700

    partially working solution for checkbox

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,29 +7,29 @@ const mql = window.matchMedia(`(min-width: 800px)`);
 
 const techStackItems = [
   'All',
-	'React',
-	'Angular',
-	'Backbone',
-	'React Native',
-	'Express',
-	'Node.js',
-	'TDD',
-	'Mocha/Chai',
-	'Redux'
+  'React',
+  'Angular',
+  'Backbone',
+  'React Native',
+  'Express',
+  'Node.js',
+  'TDD',
+  'Mocha/Chai',
+  'Redux'
 ];
 
 const rolesItems = [
   'All',
-	'Full Stack',
-	'Front-end',
-	'Back-end'
+  'Full Stack',
+  'Front-end',
+  'Back-end'
 ];
 
 const locationItems = [
   'All',
-	'San Francisco',
-	'San Jose',
-	'Palo Alto'
+  'San Francisco',
+  'San Jose',
+  'Palo Alto'
 ];
 
 class App extends Component {
@@ -39,6 +39,7 @@ class App extends Component {
       mql: mql,
       docked: props.docked,
       open: props.open,
+      isChecked: false,
       selectedTechStacksItems: [],
       selectedRolesItems: [],
       selectedLocationItems: []
@@ -46,9 +47,10 @@ class App extends Component {
     this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     this.createTechStackCheckboxes = this.createTechStackCheckboxes.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-	onSetSidebarOpen(open) {
+  onSetSidebarOpen(open) {
     this.setState({sidebarOpen: open});
   }
 
@@ -61,51 +63,79 @@ class App extends Component {
     this.setState({sidebarDocked: this.state.mql.matches});
   }
 
+
+  handleInputChange(event) {
+    const target = event.target;
+    this.setState({
+      isChecked: target
+    });
+  }
+
   createTechStackCheckboxes() {
     return (
-      techStackItems.map((item) => {
-        return (
-          <div key={item}>
-            <input type="checkbox" />
-            <label>{item}</label>
-          </div>
-        )
-      })
+      <form>
+        {techStackItems.map((item, key) => {
+          return (
+            <div key={key}>
+              <input 
+                key={key}
+                id={key}
+                type="checkbox"
+                checked={this.state.isChecked[key]}
+                onChange={this.handleInputChange} />
+              <label htmlFor={key}>{item}</label>
+            </div>
+          )
+        })}
+      </form>
     );
   }
 
-
   createRolesCheckboxes() {
-  	return (
-  		rolesItems.map((item) => {
-  			return (
-  				<div key={item}>
-  					<input type="checkbox" />
-  					<label>{item}</label>
-  				</div>
-  			)
-  		})
-		);
+    return (
+      <form>
+        {rolesItems.map((item, key) => {
+          return (
+            <div key={key}>
+              <input 
+                key={key}
+                id={key}
+                type="checkbox"
+                checked={this.state.isChecked[key]}
+                onChange={this.handleInputChange} />
+              <label htmlFor={key}>{item}</label>
+            </div>
+          )
+        })}
+      </form>
+    );
   }
 
   createLocationCheckboxes() {
-  	return (
-  		locationItems.map((item) => {
-  			return (
-  				<div key={item}>
-  					<input type="checkbox" />
-  					<label>{item}</label>
-  				</div>
-  			)
-  		})
-		);
+    return (
+      <form>
+        {locationItems.map((item, key) => {
+          return (
+            <div key={key}>
+              <input 
+                key={key}
+                id={key}
+                type="checkbox"
+                checked={this.state.isChecked[key]}
+                onChange={this.handleInputChange} />
+              <label htmlFor={key}>{item}</label>
+            </div>
+          )
+        })}
+      </form>
+    );
   }
 
   render() {
     var sidebarContent = 
     (<div>
-	    <div>Tech Stacks</div>
-			<div>
+      <div>Tech Stacks</div>
+      <div>
         {this.createTechStackCheckboxes()}
       </div>
       <br />
@@ -115,21 +145,21 @@ class App extends Component {
       </div>
       <br />
       <div>Location</div>
-     	<div>
+      <div>
         {this.createLocationCheckboxes()}
       </div>
     </div>);
 
 
     return (
-    	<div>
-	      <Sidebar 
-	        sidebar={sidebarContent}
-	        open={this.state.sidebarOpen}
-	        docked={this.state.sidebarDocked}
-	        onSetOpen={this.onSetSidebarOpen}>
+      <div>
+        <Sidebar 
+          sidebar={sidebarContent}
+          open={this.state.sidebarOpen}
+          docked={this.state.sidebarDocked}
+          onSetOpen={this.onSetSidebarOpen}>
           <MentorList />
-	      </Sidebar>
+        </Sidebar>
       </div>
     );
   }
