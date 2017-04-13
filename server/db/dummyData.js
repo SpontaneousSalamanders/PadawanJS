f6e1193de185727f20405022cbb81b93c3ffa4ca
Your Name <ddeng343@gmail.com>
2017-04-13 10:29:12 -0700

    (feat) Added roles property to mentor data

--- server/db/dummyData.js
+++ server/db/dummyData.js
@@ -4,48 +4,56 @@ module.exports = [
   {
     name: 'Beth Johnson',
     location: 'SF',
+    role: 'Front end',
     picture: 'http://imgur.com/xEUUtut.jpg',
     techStack: ['React', 'Express', 'Node.js', 'JavaScript']
   },
   {
     name: 'Benji Marinacci',
     location: 'Redwood City',
+    role: 'Back end',
     picture: 'http://imgur.com/iwn6mV5.jpg',
     techStack: ['React', 'Express', 'Node.js', 'JavaScript']
   },
   {
     name: 'Fred Zirdung',
     location: 'SF',
+    role: 'Full stack',
     picture: 'http://imgur.com/9awfsn2.jpg',
     techStack: ['React', 'Express', 'Node.js', 'JavaScript']
   },
   {
     name: 'Eric Brown',
     location: 'Albany',
+    role: 'Front end',
     picture: 'http://imgur.com/adDa5JU.jpg',
     techStack: ['React', 'Node.js', 'JavaScript']
   },
   {
     name: 'Tyler Arbus',
     location: 'Toronto',
+    role: 'Full stack',
     picture: 'http://imgur.com/ohFukM2.jpg',
     techStack: ['Angular', 'Node.js', 'JavaScript']
   },
   {
     name: 'Paul Mills',
     location: 'SF',
+    role: 'Front end',
     picture: 'http://imgur.com/9eJRSav.jpg',
     techStack: ['React', 'Node.js', 'JavaScript']
   },
   {
     name: 'Alison Zhang',
     location: 'Palo Alto',
+    role: 'Full stack',
     picture: 'http://imgur.com/uz1C3om.jpg',
     techStack: ['React', 'Node.js', 'JavaScript']
   },
   {
     name: 'Jong Kim',
     location: 'San Jose',
+    role: 'Back end',
     picture: 'http://imgur.com/yTpcGqk.jpg',
     techStack: ['React', 'Node.js', 'JavaScript']
   }
