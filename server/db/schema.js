87b9a34bd8dd31a8f9859d585ff85f15b1265726
Your Name <ddeng343@gmail.com>
2017-04-13 11:57:55 -0700

    (feat) Added resources table to schema

--- server/db/schema.js
+++ server/db/schema.js
@@ -12,27 +12,38 @@ module.exports = function(db) {
         mentor.specificType('techStack', 'text[]');
       }).then(function(table) {
         console.log('Created table!', table);
-        db.knex.schema.hasTable('events').then(function(exists) {
-          if (!exists) {
-            db.knex.schema.createTable('events', function(event) {
-              event.increments('id').primary();
-            }).then(function(table) {
-              console.log('Created table!', table);
-              db.knex.schema.hasTable('users_events').then(function(exists) {
-                if (!exists) {
-                  db.knex.schema.createTable('users_events', function(user_event) {
-                    user_event.increments('id').primary();
-                  }).then(function(table) {
-                    console.log('Created table!', table);
-                    return db.knex('mentors').insert(dummyData);
-                  }).catch(function(err) {
-                    console.log('Error creating table', err);
-                  })
-                }
-              })
+      })
+
+      db.knex.schema.hasTable('events').then(function(exists) {
+        if (!exists) {
+          db.knex.schema.createTable('events', function(event) {
+            event.increments('id').primary();
+          }).then(function(table) {
+            console.log('Created table!', table);
+            db.knex.schema.hasTable('users_events').then(function(exists) {
+              if (!exists) {
+                db.knex.schema.createTable('users_events', function(user_event) {
+                  user_event.increments('id').primary();
+                }).then(function(table) {
+                  console.log('Created table!', table);
+                  return db.knex('mentors').insert(dummyData);
+                }).catch(function(err) {
+                  console.log('Error creating table', err);
+                })
+              }
             })
-          }
-        })
+          })
+        }
+      })
+
+      db.knex.schema.hasTable('resources').then(function(exists) {
+        if (!exists) {
+          db.knex.schema.createTable('resources', function(resource) {
+            resource.increments('id').primary();
+          }).then(function(table) {
+            console.log('Created table!', table);
+          })
+        }
       })
     }
   });
