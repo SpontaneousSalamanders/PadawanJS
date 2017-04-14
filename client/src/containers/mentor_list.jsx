2e93acec98e31b88982c8eca8768c7430c7ff7f7
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-04-14 11:23:32 -0700

    css updates

--- client/src/containers/mentor_list.jsx
+++ client/src/containers/mentor_list.jsx
@@ -4,20 +4,12 @@ import { Card, CardTitle } from 'react-materialize';
 import { selectMentor } from '../actions/index.jsx'
 import { bindActionCreators } from 'redux';
 import MentorProfile from './mentor_profile.jsx';
-<<<<<<< HEAD
 import { Link } from 'react-router';
-=======
->>>>>>> Add mentor profile page and with action and reducers
 
 class MentorList extends Component {
   renderList() {
     return this.props.mentors.map((mentor) => {
       return (
-<<<<<<< HEAD
-        <div style={{marginTop: 100}}>
-=======
-        <div>
->>>>>>> Add mentor profile page and with action and reducers
         <Card 
         key={mentor.name}
         header={<CardTitle reveal image={mentor.picture} waves='light'/>}
@@ -29,23 +21,15 @@ class MentorList extends Component {
                   <li>Location: {mentor.location}</li>
                   <li>Expertise: {mentor.techStack.join(', ')}</li>
                 </ul>
-<<<<<<< HEAD
                 <Link
                   to="/profile"
                   onClick={()=> this.props.selectMentor(mentor)}
                   className="viewProfileButton">View Profile
                 </Link>
-=======
-                <button 
-                  onClick={()=> this.props.selectMentor(mentor)}
-                  className="viewProfileButton">View Profile
-                </button>
->>>>>>> Add mentor profile page and with action and reducers
                 <button className="requestLightSaberButton">Request Lightsaber</button>
               </div>
             }>
         </Card>
-        </div>
       )
     })
   }
