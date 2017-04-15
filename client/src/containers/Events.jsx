4a7bde79374aee0b6ad66631c503ba8a393f053f
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-04-14 19:36:40 -0700

    Add css to Events container

--- client/src/containers/Events.jsx
+++ client/src/containers/Events.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getEvents } from '../actions/eventActions.jsx';
+import { Divider } from 'semantic-ui-react';
 
 class Events extends Component {
   componentDidMount() {
@@ -12,20 +13,34 @@ class Events extends Component {
     return this.props.events.length > 0 ?
     (
       <div>
-        Events:
-        {
-          this.props.events.map((event) => {
-            return (
-              <ul>
-                <li>{event.title}</li>
-                <li>{event.description}</li>
-                <li>{event.location}</li>
-                <li>{event.date}</li>
-                <li>{event.time}</li>
-              </ul>
-            )
-          })
-        }
+      <h4 style={{textAlign: 'center'}}>Events</h4>
+      <Divider />
+      <ul className="media-list">
+        {this.props.events.map((event, index)=>{
+          return (
+            <li key={index} className="media">
+              <div className="media-left">
+                <div
+                  style={{width: 50, cursor: 'pointer'}}
+                  className='thumbnail'
+                  >
+                  <img className="media-object" src='https://d30y9cdsu7xlg0.cloudfront.net/png/89454-200.png' alt="..."/>
+                </div>
+              </div>
+              <div className="media-body">
+                <h5
+                  style={{cursor: 'pointer'}}
+                
+                  className="media-heading" >{event.title}</h5>
+                <p>
+                  {event.description}
+                </p>
+              </div>
+            </li>
+          )
+        })}
+
+      </ul>
       </div>
     )
     :
