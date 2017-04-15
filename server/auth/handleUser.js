71c672b04df96411750947be096a1eacfaf6dd88
Chris Chu <cchu008@gmail.com>
2017-04-15 16:34:48 -0700

    clean up syntax errors

--- server/auth/handleUser.js
+++ server/auth/handleUser.js
@@ -1,13 +1,17 @@
-var db = require('./db/index.js');
+// You can even use knex without a connection, just for its query building features. Just pass in an empty object when initializing the library. Specify a client if you are interested in a particular flavour of SQL.
+
+var db = require('../db');
 var bcrypt = require('bcrypt');
+
+// var knex = require('knex');
+// var pg = require('knex')({client: 'pg'});
 
 // Before inserting a user, encrypt the password
 
 function createUser (email, password, name) {
   const salt = bcrypt.genSaltSync();
   const hash = bcrypt.hashSync(password, salt);
-  return db.knex('users')
-  .insert({
+  return db.knex('users').insert({
     email: email,
     password: hash,
     name: name
@@ -16,39 +20,40 @@ function createUser (email, password, name) {
 }
 
 
-function comparePass(userPassword, databasePassword) {
+function comparePass (userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
 
 // check to see if email username is already in database
 // if not, return false
 // else, return the user
-function checkUsernameInDB(email, password, done) {
-  return db.knex('users').where({ email }).first()
-    .then((user)) => {
-      if( !user) return done(null, false);
-      if( !comparePass(password, user.password)) {
-        return done(null, false);
-      } else {
-        return done(null, user);
-      }
-    })
-    .catch((err) => { return done(err); })
+function checkUsernameInDB (email, password, done) {
+  db.knex('users').where({ email }).first()
+  .then((user) => {
+    if(!user) return done(null, false);
+    if(!comparePass(password, user.password)) {
+      return done(null, false);
+    } else {
+      return done(null, user);
+    }
+  })
+  .catch((err) => { return done(err); })
 }
 
 function checkIdInDB(id) {
   return db.knex('users').where({ id }).first()
-  .then((user)) => {
+  .then((user) => {
     if (!user) return done (null, false);
     else {
       return done (null, user);
     }
-  }
+  })
 }
 
 
 module.exports = {
   comparePass,
   createUser,
-  checkUsernameInDB
+  checkUsernameInDB,
+  checkIdInDB
 }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,12 +3,12 @@ const auth = require ('../auth/controllers/authentication');
 
 // Passport middle modele and setup
 const passport = require('passport');
-const passportStrategies = require('./passportStrategies');
+const passportStrategies = require('../auth/passportStrategies');
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 //Custom express routing middleware that checks to see if the authenticated user is an admin
-const requireMentor = require('./requireMentor')
+const requireMentor = require('../auth/requireMentor')
 
 module.exports = function(app) {
 
