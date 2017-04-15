8243671861aac76717bd0a26c45215471986070a
Chris Chu <cchu008@gmail.com>
2017-04-14 17:06:59 -0700

    login form renders but need to set up action

---
+++ client/src/containers/auth/LoginForm.jsx
@@ -0,0 +1,60 @@
+/**
+ * LoginForm.jsx
+ *
+ * The form with a username and a password input field, both of which are
+ * controlled via the application state.
+ *
+ */
+
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+
+
+class LoginForm extends Component {
+  handleFormSubmit({ email, password }) {
+    this.props.signinUser({ email, password });
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
+  render() {
+    const { handleSubmit, fields: { email, password }} = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} placeholder="fred.zirdung@hackreactor.com" className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" placeholder="••••••••••" className="form-control" />
+        </fieldset>
+        {this.renderAlert()}
+        <button action="submit" className="btn btn-primary">Sign in</button>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
+export default reduxForm({
+  form: 'login',
+  fields: ['email', 'password']
+}, mapStateToProps, actions)(LoginForm);
+
+
+
