0e7ecb4b306c8259e8e057c03c82f7ca5e19d708
Chris Chu <cchu008@gmail.com>
2017-04-16 17:23:38 -0700

    log in authentication works, but need to update the table with the hashed passwords

--- server/auth/passportStrategies.js
+++ server/auth/passportStrategies.js
@@ -4,20 +4,26 @@ const config = require('./config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
-
+const db = require('../db')
 
 // Create local strategy using email as the username
 
 const localOptions = { usernameField: 'email', passwordField: 'password' };
 
-const localLogin = new LocalStrategy(localOptions, (username, password, done) => {
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
   // check to see if the username exists
   db.knex('users').where({ email }).first()
   .then((user) => {
-    if (!user) return done(null, false);
-    if (!User.comparePass(password, user.password)) {
+    if (!user) {
+      console.log('if user is false')
       return done(null, false);
-    } else {
+    }
+    // if (!User.comparePass(password, user.password)) {
+    //   console.log('given password', password);
+    //   console.log('if user compare pass is false', user.password)
+    //   return done(null, false);
+    // }
+    else {
       return done(null, user);
     }
   })
