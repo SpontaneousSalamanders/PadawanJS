c2cf2a56a39471ea923f79f5b8c48f15dde3c3a0
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-04-17 12:10:03 -0700

    client/src/components/HomePage.jsx

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import SidebarMenu from '../containers/SidebarMenu.jsx';
 import MentorList from '../containers/mentor_list.jsx';
+import SidebarMenu from '../containers/SidebarMenu.jsx';
+import Events from '../containers/Events.jsx';
 
 function HomePage() {
   return (
     <div >
       <SidebarMenu />
       <MentorList />
+      <Events />
     </div>
   );
 }
