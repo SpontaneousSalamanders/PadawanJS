d48c07f73205b6bad5b63f2e00e17b7c4bb6ff11
Chris Chu <cchu008@gmail.com>
2017-04-17 14:56:05 -0700

    update register form

--- client/src/containers/auth/RegisterForm.jsx
+++ client/src/containers/auth/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 import * as actions from '../../actions/authActions.jsx';
 
 class RegisterForm extends Component {
@@ -20,6 +20,7 @@ class RegisterForm extends Component {
   render() {
     const { handleSubmit, fields: { email, password, firstName, lastName, passwordConfirm }} = this.props;
 
+    // renders the sign up register form
     return (
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
@@ -37,45 +38,20 @@ class RegisterForm extends Component {
         <fieldset className="form-group">
           <label>Password:</label>
           <input className="form-control" {...password} type="password" />
-
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input className="form-control" {...passwordConfirm} type="password" />
-
         </fieldset>
+
+
+
         {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign up!</button>
       </form>
     );
   }
 }
-
-// function validate(formProps) {
-//   const errors = {};
-
-//   if (!formProps.firstName || !formProps.lastName) {
-//     errors.firstName = 'Please enter a name';
-//     errors.lastName = 'Please enter a name';
-
-//   if (!formProps.email) {
-//     errors.email = 'Please enter an email';
-//   }
-
-//   if (!formProps.password) {
-//     errors.password = 'Please enter a password';
-//   }
-
-//   if (!formProps.passwordConfirm) {
-//     errors.passwordConfirm = 'Please enter a password confirmation';
-//   }
-
-//   if (formProps.password !== formProps.passwordConfirm) {
-//     errors.password = 'Passwords must match';
-//   }
-
-//   return errors;
-// }
 
 function mapStateToProps(state) {
   return { errorMessage: state.auth.error };
