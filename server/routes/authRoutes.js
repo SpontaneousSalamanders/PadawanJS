27fcbbe2884ba622a1b34e65f040b72394528416
Chris Chu <cchu008@gmail.com>
2017-04-18 17:30:13 -0700

    add requireMentor to authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -17,31 +17,23 @@ module.exports = function(app) {
 
   // using requireAuth passport middleware w/ jwt strategy to protect route
 
-  app.get('/student_profile/:id', requireAuth, function(req, res) {
+  app.get('/student_profile/', requireAuth, function(req, res) {
     res.send({ message: 'server response: this GET request has been authorized for a padawan' });
   });
 
   // using requireAuth passport middleware w/ jwt strategy as well as requireAdmin custom express middleware to protect route
   // must be a mentor to access mentor area
 
-  app.get('/mentor_profile/:id', requireAuth, requireMentor, function(req, res, next) {
+  app.get('/mentor_profile/', requireAuth, requireMentor, function(req, res, next) {
     res.send({ message: 'server response: this GET request has been authorized for a mentor' });
   })
 
   // using requireSignin passport middleware to authenticate for protected route using local (email/password) strategy)
+
   // Authentication.signin sends back JWT token to authenticated user
 
   app.post('/signin', requireSignin, auth.signin);
 
-  // // manually update this person's password in the DB and then redirect them to homepage
-
-  // app.post('/signin', function(req, res) {
-  //   User.createHashAndInsertToDB (req.body.email, req.body.password);
-
-  //   console.log(db.knex('users').where('email', req.body.email));
-
-  //   res.redirect('/');
-  // })
 
   // route for signing up user
   app.post('/signup', auth.signup);
