85f04e1a533ee325722993a58ee928298d88a3ea
Your Name <ddeng343@gmail.com>
2017-04-19 12:01:44 -0700

    Created route to fetch questions

--- server/routes/getQuestions.js
+++ server/routes/getQuestions.js
@@ -1,15 +1,15 @@
-// 'use strict';
+'use strict';
 
-// const Resources = require('../models/resources.js');
+const Messages = require('../models/messages.js');
 
-// module.exports = (req, res) => {
-//   const user_id = req.params.uid;
+module.exports = (req, res) => {
+  const user_id = req.params.uid;
 
-//   Resources.getMentorResources(user_id)
-//   .then((resources) => {
-//     res.status(200).send(resources);
-//   })
-//   .catch((err) => {
-//     res.status(err.status || 500).send({'error in getMentorResources': err});
-//   });
-// }
+  Messages.getQuestions(user_id)
+  .then((questions) => {
+    res.status(200).send(questions);
+  })
+  .catch((err) => {
+    res.status(err.status || 500).send({'error in getQuestions': err});
+  });
+}
