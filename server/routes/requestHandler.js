f353b3f6b5ac78cdcae4509ae6318ee34b3fed63
Your Name <ddeng343@gmail.com>
2017-04-19 12:01:44 -0700

    Created route to fetch questions

--- server/routes/requestHandler.js
+++ server/routes/requestHandler.js
@@ -6,7 +6,7 @@ module.exports = {
   getMentors: require('./getMentors.js'),
   getMentorResources: require('./getMentorResources.js'),
   getMenteeResources: require('./getMenteeResources.js'),
-  getQuestions: require('./getQuestions.js')
+  getQuestions: require('./getQuestions.js'),
   postEvent: require('./postEvent.js'),
   postResource: require('./postResource.js'),
   postQuestion: require('./postQuestion.js'),
