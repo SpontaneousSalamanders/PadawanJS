c409f3771f91c17c98ea399b98995af246eb5120
Your Name <ddeng343@gmail.com>
2017-04-19 16:41:00 -0700

    Created route to fetch padawan event data

---
+++ server/routes/getMenteeEvents.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Events = require('../models/events.js');
+
+module.exports = (req, res) => {
+  // Events.getMenteeEvents()
+  // .then((resources) => {
+  //   res.status(200).send(resources);
+  // })
+  // .catch((err) => {
+  //   res.status(err.status || 500).send({'error in getResources': err});
+  // });
+}
