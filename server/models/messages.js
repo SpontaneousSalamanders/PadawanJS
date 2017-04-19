ef20c0b64b424a88e8e536c587ee37d585429861
Your Name <ddeng343@gmail.com>
2017-04-19 11:13:15 -0700

    Set up server-side file structure for message feature

---
+++ server/models/messages.js
@@ -0,0 +1,33 @@
+// const db = require('../db');
+
+// module.exports = {
+//   getMentorResources: (user_id) => {
+//     return db.knex
+//     .select('title', 'description', 'URL', 'icon')
+//     .from('resources')
+//     .where({user_id: user_id});
+//   },
+
+//   getMenteeResources: (user_id) => {
+//     //
+//   },
+
+//   postResource: (resource) => {
+//     return db.knex('resources')
+//     .insert({
+//       user_id: resource.user_id,
+//       title: resource.title,
+//       description: resource.description,
+//       URL: resource.url,
+//       category: resource.category
+//     });
+//   },
+
+//   saveResource: (user_id, resource_id) => {
+//     return db.knex('users_resources')
+//     .insert({
+//       user_id: user_id,
+//       resource_id: resource_id
+//     });
+//   }
+// };
---
+++ server/routes/getQuestions.js
@@ -0,0 +1,15 @@
+// 'use strict';
+
+// const Resources = require('../models/resources.js');
+
+// module.exports = (req, res) => {
+//   const user_id = req.params.uid;
+
+//   Resources.getMentorResources(user_id)
+//   .then((resources) => {
+//     res.status(200).send(resources);
+//   })
+//   .catch((err) => {
+//     res.status(err.status || 500).send({'error in getMentorResources': err});
+//   });
+// }
