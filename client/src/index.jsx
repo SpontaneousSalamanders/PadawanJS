c280ac92f7545a1875093720501581100b293793
Chris Chu <cchu008@gmail.com>
2017-04-19 10:30:31 -0700

    building dashboard changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,14 +6,33 @@ import { Router, browserHistory } from 'react-router'
 import routes from './routes.jsx'
 import ReduxPromise from 'redux-promise';
 import thunk from 'redux-thunk'
-
 import App from './components/App.jsx';
 import reducers from './reducers/index.jsx';
+import { AUTH_USER } from './actions/authActions.jsx';
+import { SET_MENTOR_PRIVILEGES } from './actions/authActions.jsx';
+import jwt_decode from 'jwt-decode';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise, thunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// update application state with token information if needed
+if (token) {
+  // update authentication flag
+  store.dispatch({ type: AUTH_USER });
+
+  // update mentor privileges if needed
+  let decoded_token = jwt_decode(token);
+
+  if (decoded_token.type === 'mentor') {
+    store.dispatch({ type: SET_MENTOR_PRIVILEGES });
+  }
+}
+
+
 
 render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.getElementById('app')
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -7,6 +7,7 @@ import FindMentor from './components/FindMentor.jsx';
 import LoginPage from './containers/LoginPage.jsx';
 import SignUpPage from './containers/SignUpPage.jsx';
 import MentorPage from './components/MentorPage.jsx'
+import Signout from './containers/auth/Signout.jsx'
 
 
 export default (<Route component={App} >
@@ -14,6 +15,7 @@ export default (<Route component={App} >
   <Route path="/find_mentor" component={FindMentor} />
   <Route path="/signin" component={LoginPage} />
   <Route path="/signup" component={SignUpPage} />
+  <Route path="/signout" component={Signout} />
   <Route path="/profile/*" component={MentorPage} />
   </Route>
 );
