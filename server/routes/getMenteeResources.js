706c766fcf44cc4fd977586df4c308b3976e1591
Your Name <ddeng343@gmail.com>
2017-04-19 17:49:00 -0700

    Finished query to fetch mentee's save resources

--- server/routes/getMenteeResources.js
+++ server/routes/getMenteeResources.js
@@ -3,11 +3,13 @@
 const Resources = require('../models/resources.js');
 
 module.exports = (req, res) => {
-  // Resources.getMenteeResources()
-  // .then((resources) => {
-  //   res.status(200).send(resources);
-  // })
-  // .catch((err) => {
-  //   res.status(err.status || 500).send({'error in getResources': err});
-  // });
+  const user_id = req.params.uid;
+
+  Resources.getMenteeResources(user_id)
+  .then((resources) => {
+    res.status(200).send(resources);
+  })
+  .catch((err) => {
+    res.status(err.status || 500).send({'error in getMenteeResources': err});
+  });
 }
