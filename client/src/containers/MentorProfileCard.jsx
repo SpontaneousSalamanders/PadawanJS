a41b46b1ad8aebf6bdc088c5b73b527b5c830831
brian <briandjin@gmail.com>
2017-04-20 16:34:34 -0700

    Dispaly eventForm in mentorPage

--- client/src/containers/MentorProfileCard.jsx
+++ client/src/containers/MentorProfileCard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
 
 
 function MentorCard (props) {
