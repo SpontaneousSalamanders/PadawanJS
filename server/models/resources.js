d1d1e0414f3f0b7d03de9cfe963f49d855e65456
Your Name <ddeng343@gmail.com>
2017-04-19 17:49:00 -0700

    Finished query to fetch mentee's save resources

--- server/models/resources.js
+++ server/models/resources.js
@@ -10,7 +10,14 @@ module.exports = {
   },
 
   getMenteeResources: (user_id) => {
-    //
+    return db.knex
+    .select()
+    .from('users')
+    .innerJoin('users_resources', function() {
+      this
+      .on('users_resources.user_id', '=', Number(user_id))
+      .andOn('resources.id', '=', 'users_resources.resource_id');
+    });
   },
 
   postResource: (resource) => {
