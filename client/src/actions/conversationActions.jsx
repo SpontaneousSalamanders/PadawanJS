807fe4caded18170ba4c8c365066a2c125095179
Chris Chu <cchu008@gmail.com>
2017-04-21 20:30:10 -0700

    include token in request

--- client/src/actions/conversationActions.jsx
+++ client/src/actions/conversationActions.jsx
@@ -3,7 +3,9 @@ import axios from 'axios';
 export const GET_ALL_CONVERSATIONS = 'GET_ALL_CONVERSATIONS';
 
 export function getConversations(user_id) {
-  const request = axios.get('/conversations' + user_id);
+  const request = axios.get('/conversations', { headers: { authorization: localStorage.getItem('token') }
+    })
+
 
   return {
     type: GET_ALL_CONVERSATIONS,
--- client/src/containers/Inbox.jsx
+++ client/src/containers/Inbox.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getConversations } from '../actions/conversationActions.jsx'
 import { bindActionCreators } from 'redux';
 
 class Inbox extends Component {
   componentDidMount() {
-
+    this.props.getConversations
   }
 }
