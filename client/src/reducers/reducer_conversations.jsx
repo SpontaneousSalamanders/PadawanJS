5bc9dbb024856b566b5a973ff3fb40f0f8db0db3
Chris Chu <cchu008@gmail.com>
2017-04-22 11:58:51 -0700

    select conversation reducer

--- client/src/reducers/reducer_conversations.jsx
+++ client/src/reducers/reducer_conversations.jsx
@@ -1,10 +1,16 @@
-import { GET_ALL_CONVERSATIONS } from '../actions/conversationActions.jsx';
+import {
+  GET_ALL_CONVERSATIONS,
+  SELECT_CONVERSATION,
+
+} from '../actions/conversationActions.jsx';
 
 export default function(state = convoData: [], action) {
   switch (action.type) {
     case GET_ALL_CONVERSATIONS:
-    return Object.assign({}, state, {conversations: action.payload.data})
+      return { ...state, conversations: action.payload };
+    case SELECT_CONVERSATION:
+      return { ...state, conversation:
+        action.payload }
   }
-
     return state;
 }
