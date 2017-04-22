f8b595a9014b0de34487e79979131aea0354186f
Chris Chu <cchu008@gmail.com>
2017-04-21 19:29:14 -0700

    created a DM actions and building out Inbox container

---
+++ client/src/actions/directmessageActions.jsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const GET_DIRECT_MESSAGES = 'GET_DIRECT_MESSAGES';
---
+++ client/src/containers/Inbox.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class Inbox extends Component {
+  componentDidMount() {
+
+  }
+}
