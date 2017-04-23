08bce259d4312c8c2246d9e855a700df4ae807e9
Your Name <ddeng343@gmail.com>
2017-04-23 14:26:34 -0700

    Updated seed data to include more threaded messages

--- server/db/dummy/messages.js
+++ server/db/dummy/messages.js
@@ -33,5 +33,38 @@ module.exports.messages = [
     user_id: 1,
     message: 'What is the time complexity of bubble sort?',
     created_at: '2017-04-19 12:33:17.205319-07'
+  },
+  {
+    title: 'Answer these questions three',
+    user_id: 2,
+    message: 'What is the airspeed velocity of an unladen swallow?',
+    created_at: '2017-04-19 12:32:17.205319-07'
+  },
+  {
+    user_id: 5,
+    message: 'What do you mean? African or European swallow?',
+    reply_to_message_id: 6,
+    root_message_id: 1,
+    created_at: '2017-04-19 12:33:17.205319-07'
+  },
+  {
+    user_id: 6,
+    message: 'Nooooooo!',
+    reply_to_message_id: 7,
+    root_message_id: 1,
+    created_at: '2017-04-19 12:34:17.205319-07'
+  },
+  {
+    user_id: 7,
+    message: 'What?',
+    reply_to_message_id: 6,
+    root_message_id: 1,
+    created_at: '2017-04-19 12:35:17.205319-07'
+  },
+  {
+    title: 'Answer these questions three',
+    user_id: 2,
+    message: 'What is your quest?',
+    created_at: '2017-04-19 12:33:17.205319-07'
   }
 ]
