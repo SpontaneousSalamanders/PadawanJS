844ef954ac9e0039d005e6f0eda58a7f7048b510
Chris Chu <cchu008@gmail.com>
2017-04-24 11:08:15 -0700

    change to camel casing, continue work on direct messages

--- client/src/actions/directmessageActions.jsx
+++ client/src/actions/directmessageActions.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const GET_DIRECT_MESSAGES = 'GET_DIRECT_MESSAGES';
 
 export function getConversation (conversation_id) {
+  console.log('getconvo is being called');
   const request = axios.get('/conversation' + conversation_id);
 
   return {
