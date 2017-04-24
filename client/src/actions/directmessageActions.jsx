8950a65dcc2101bf01425525a3e80f8df37396f6
Chris Chu <cchu008@gmail.com>
2017-04-24 12:28:29 -0700

    able to access direct messages from database

--- client/src/actions/directmessageActions.jsx
+++ client/src/actions/directmessageActions.jsx
@@ -4,7 +4,9 @@ export const GET_DIRECT_MESSAGES = 'GET_DIRECT_MESSAGES';
 
 export function getConversation (conversation_id) {
   console.log('getconvo is being called', conversation_id);
-  const request = axios.get('/conversation' + conversation_id);
+  const request = axios.get('/conversation/' + conversation_id, { headers: {
+    authorization: localStorage.getItem('token') }
+  })
 
 
   return {
