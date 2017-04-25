769cbe3f537db5d2f05eb12241a77bf7a7218942
Chris Chu <cchu008@gmail.com>
2017-04-25 06:08:07 -0700

    built a start conversation and post direct message in the back end

--- server/routes/startConversation.js
+++ server/routes/startConversation.js
@@ -4,3 +4,16 @@ const DirectMessages = require('../models/directMessages.js');
 
 module.exports = (req, res) => {
   const message = req.body;
+
+  DirectMessages.startConversation()
+  .then(() => {
+    DirectMessages.postDirectMessage(message)
+    .then( () => {
+      res.status(200).end();
+    })
+  })
+  .catch((err) => {
+    res.status(err.status || 500).send({'error in startConversation': err
+    });
+  });
+}
