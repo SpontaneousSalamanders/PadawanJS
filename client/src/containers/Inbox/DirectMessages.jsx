9e6972d068269e58601bb759cd4a8fb34ea2afd7
Chris Chu <cchu008@gmail.com>
2017-04-25 16:11:24 -0700

    revert back to previous direct message

--- client/src/containers/Inbox/DirectMessages.jsx
+++ client/src/containers/Inbox/DirectMessages.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
-import { getConversation, sendMessage, fieldInput } from '../../actions/directMessageActions.jsx'
+import { getConversation } from '../../actions/directMessageActions.jsx'
 import SingleMessage from './SingleMessage.jsx';
-import { Field, reduxForm } from 'redux-form';
-import axios from 'axios'
-
+// import { submitMessage, fieldInput } from '../../actions/directmessageActions.jsx'
 
 
 class DirectMessages extends Component {
@@ -26,35 +24,9 @@ class DirectMessages extends Component {
   }
 
   render() {
-
-    const handleSubmit = (props) => {
-      console.log('inside handleSubmit', props);
-      axios.post('/directMessage/', props, { headers: {
-    authorization: localStorage.getItem('token') }
-      });
-    }
-
-    const handleChange = (e) => {
-      e.preventDefault();
-      this.props.fieldInput(e.target.value)
-    }
-
-
-
     return (
-      <div>
       <div className="container" style={{width: '100%', marginTop: 100, marginLeft: 150}}>
-
         {this.renderMessages()}
-
-        <label>Message:</label>
-
-          <textarea type="text" value={this.props.field} onChange={(e) => {this.props.fieldInput(e.target.value)}}/>
-
-          <button onClick={() => this.handleSubmit(this.props.field)}>
-            Send Message
-          </button>
-      </div>
       </div>
     )
   }
@@ -65,17 +37,12 @@ function mapStateToProps(state) {
   return {
     conversations: state.conversations,
     directMessages: state.directMessages,
-    mentor: state.selectedMentor,
-    field: state.field,
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
-    getConversation: getConversation,
-    sendMessage: sendMessage,
-    fieldInput: fieldInput
-  }, dispatch);
+    getConversation: getConversation}, dispatch);
 }
 
 
