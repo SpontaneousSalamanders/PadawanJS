c53f7fe531d2510940dabe3ffb35023d874292d6
brian <briandjin@gmail.com>
2017-04-26 16:22:21 -0700

    Reformat reducer tests

--- test/test.js
+++ test/test.js
@@ -7,28 +7,44 @@ import SavedResourcesReducer from '../client/src/reducers/reducer_saved_resource
 import SelectedMentorReducer from '../client/src/reducers/reducer_selected_mentor.jsx';
 
 
-describe('Reducers', () => {
+describe('Event Reducer', () => {
   it('Events has a default state', () => {
     expect(EventsReducer(undefined, { type: 'unexpected' })).toEqual({ eventData: [] });
   });
+});
+
+describe('Mentors Reducers', () => {
   it('Mentors has a default state', () => {
     expect(MentorsReducer(undefined, { type: 'unexpected' })).toEqual({ index: [], filtered: [] });
   });
+});
+
+describe('Reducers', () => {
   it('Message has a default state', () => {
     expect(MessageReducer(undefined, { type: 'unexpected' })).toEqual({});
   });
+});
+
+describe('Resources Reducers', () => {
   it('Resources has a default state', () => {
     expect(ResourcesReducer(undefined, { type: 'unexpected' })).toEqual({ resourceData: [] });
   });
+});
+
+describe('Saved Events Reducers', () => {
   it('Saved Events has a default state', () => {
     expect(SavedEventsReducer(undefined, { type: 'unexpected' })).toEqual({ savedEventsData: [] });
   });
+});
+
+describe('Saved Resources Reducers', () => {
   it('Saved Resources has a default state', () => {
     expect(SavedResourcesReducer(undefined, { type: 'unexpected' })).toEqual({ savedResourcesData: [] });
   });
+});
+describe('Selected Mentor Reducers', () => {
   it('Selected Mentor has a default state', () => {
     expect(SelectedMentorReducer(undefined, { type: 'unexpected' })).toEqual(null);
   });
-  
 });
-
+  
