ea00e5c46abbd34d30f8e29cc78f7b0cff2d2a6d
Chris Chu <cchu008@gmail.com>
2017-04-27 16:53:48 -0700

    scale back experimental bug fix to get stable version

--- client/src/actions/postEventActions.jsx
+++ client/src/actions/postEventActions.jsx
@@ -15,17 +15,24 @@ export function postEvent(props) {
     .then( (response) => {
       dispatch({ type: POST_EVENT })
 
-      let decoded_token_data = jwt_decode(response.data.token);
+      // let decoded_token_data = jwt_decode(response.data.token);
 
       // - Save the JWT token
       // localStorage.setItem('token', response.data.token);
 
       console.log('response is:', decoded_token_data)
 
+
       // browserHistory.push(`/profile/${decoded_token_data.id}`)
     })
     .catch( (response) => {
       console.log(response)
+
+      // browserHistory.push(`/profile/${response.data.id}`)
+    })
+    .catch( (response) => {
+      console.log(response)
+      // dispatch(authError(response.data.error))
     })
   }
   // return {
